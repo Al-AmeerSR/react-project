673eac8d73751d414b3ad787580844f972d33025
Al-AmeerSR <iamameer37@gmail.com>
2024-09-03 23:26:05 +0530

    adding form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-
+import React,{useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodTable';
-
+import NewTodoForm from './components/NewTodoForm';
 function App() {
 
-  const todos =[
+  const [todos,setTodos] =useState([
     { rowNumber : 1,rowDescription:'Feed the puppy',rowAssignee:'user1'},
     {rowNumber : 2,rowDescription:'Read book',rowAssignee:'user2'},
     {rowNumber : 3,rowDescription:'Cook dinner',rowAssignee:'user1'},
     {rowNumber : 4,rowDescription:'charge phone',rowAssignee:'user2'}
-  ]
+  ])
 
   const addTodo = ()=>{
- 
+ if(todos.length>0){
     const newTodo = {rowNumber : todos.length + 1 ,rowDescription:'charge the laptop',rowAssignee :'user3'}
    
-   todos.push(newTodo)
+  setTodos(todos => [...todos,newTodo]);
    console.log(todos)
+  }
   }
   return (
     <div className="mt-5 container">
@@ -30,6 +31,7 @@ function App() {
           <button className='btn btn-primary' onClick={addTodo}>
             Add New todo
             </button>
+            <NewTodoForm/>
         </div>
       </div>
     </div>
